fa1cac2c72e0780a461b4f9fb69ca41548c4ae88
肖志斌 <zhibin.xiao@beibei.com>
2019-01-09 14:47:43 +0800

    添加willDisplayCellHandler 和 didEndDisplayingCellHandler 回调,增加api

--- ZBFancyCollectionView.podspec
+++ ZBFancyCollectionView.podspec
@@ -1,7 +1,7 @@
 #
 Pod::Spec.new do |s|
   s.name = 'ZBFancyCollectionView'
-  s.version = '0.3.2'
+  s.version = '0.3.3'
   s.summary = 'A delightful iOS ZBFancyCollectionView framework.'
   s.homepage = 'https://github.com/k373379320/ZBFancyCollectionView'
   s.license = { :type => 'MIT', :file => 'LICENSE' }
--- ZBPushToGitHub.sh
+++ ZBPushToGitHub.sh
@@ -3,8 +3,8 @@
 echo "pod update..."
 
 git add .
-git commit  -m "添加willDisplayCellHandler 和 didEndDisplayingCellHandler 回调"
-git tag "0.3.2"
+git commit  -m "添加willDisplayCellHandler 和 didEndDisplayingCellHandler 回调,增加api"
+git tag "0.3.3"
 git push origin master
 git push --tags
 pod trunk push ZBFancyCollectionView.podspec
