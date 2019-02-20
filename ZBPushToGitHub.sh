4fbbddc531ddccc684cae30a6568c0d8c0d090d3
肖志斌 <zhibin.xiao@beibei.com>
2019-02-20 18:51:43 +0800

    回退

--- ZBPushToGitHub.sh
+++ ZBPushToGitHub.sh
@@ -3,8 +3,8 @@
 echo "pod update..."
 
 git add .
-git commit  -m "添加willDisplayCellHandler 和 didEndDisplayingCellHandler 回调,增加api"
-git tag "0.3.3"
+git commit  -m "恢复"
+git tag "1.1.0"
 git push origin master
 git push --tags
 pod trunk push ZBFancyCollectionView.podspec
