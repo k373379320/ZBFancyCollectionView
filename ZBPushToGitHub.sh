16cd14fc9cac1be6717cfe9b04d8b58811e5802b
志斌_肖 <373379320@qq.com>
2023-12-09 14:45:03 +0800

    feat:更新提交脚本

--- ZBPushToGitHub.sh
+++ ZBPushToGitHub.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
-
+#提交有问题的话,需要使用SSH下载项目
 echo "pod update..."
-
 git add .
-git commit  -m "fix:兼容改为最低12"
-git tag "1.2.4"
+git commit  -m "fix:header高度问题"
+git tag "1.2.3"
 git push origin master
 git push --tags
-pod trunk push ZBFancyCollectionView.podspec
+pod trunk push ZBFancyCollectionView.podspec --allow-warnings
