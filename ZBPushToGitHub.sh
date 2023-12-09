102235b531b0f3cadeec779afefcf9bf4e06122a
志斌_肖 <373379320@qq.com>
2023-12-09 14:03:16 +0800

    1.2.3

--- ZBPushToGitHub.sh
+++ ZBPushToGitHub.sh
@@ -4,7 +4,7 @@ echo "pod update..."
 
 git add .
 git commit  -m "fix:header高度问题"
-git tag "1.2.2"
+git tag "1.2.3"
 git push origin master
 git push --tags
 pod trunk push ZBFancyCollectionView.podspec
